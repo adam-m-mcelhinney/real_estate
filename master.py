8f72400a390ce9952714abd04c865ff596d65161
adam-m-mcelhinney <adam.m.mcelhinney@gmail.com>
2013-03-08 11:21:38 -0600

    Update master.py
    Changed handling of error records to be more readable. Did some basic debugging.

--- master.py
+++ master.py
@@ -5,20 +5,21 @@ import time
 import random
 
 # Get the file
-file='C:/Users/Adam/Documents/GitHub/real_estate/test_data.csv'
+#file='C:/Users/Adam/Documents/GitHub/real_estate/test_data.csv'
+file='C:/Local Files/real_estate-master/real_estate-master/test_data.csv'
 nav_root='http://www.zillow.com/'
 search_root='http://www.zillow.com/homes/'
 
 data=import_clean(file)
-data=data[:5]
+#data=data[:5]
 
 # loop through the addresses and get the search urls
 search=[]
 for i in range(len(data)):
     #print i
     # Dont hit the server too fast
-    #pause=random.random()+random.randint(0,1)
-    #time.sleep(pause)
+    pause=random.random()+random.randint(1,3)
+    time.sleep(pause)
     
     search.append(search_prop(nav_root,search_root,data[i]))
 
@@ -28,20 +29,25 @@ out=[['Address','Baths','Beds','Zestimate','Rental Estimate','Year Built','Lot S
 
 for i in range(len(data)):
     # Dont hit the server too fast
-    #pause=random.random()+random.randint(0,1)
-    #time.sleep(pause)
-    #print search[i]
+    pause=random.random()+random.randint(1,3)
+    time.sleep(pause)
+   
     try:
+        
         row=get_info(search[i])
         row.insert(0,data[i])
         
     except:
-        row=[data[i],search[i]]
-        out.append(row)
+        row=(len(out[0])-2)*['NA']
+        row.append(search[i])
+        row.insert(0,data[i])
+        #row=[data[i],'','','','','','','','',search[i]]
+
+    out.append(row)
 
 
 import csv
-with open('C:/Users/Adam/Documents/GitHub/real_estate/out_data.csv', 'wb') as f:
+with open('C:/Local Files/real_estate-master/real_estate-master/out_data.csv', 'wb') as f:
     writer = csv.writer(f)
     writer.writerows(out)
         
