08c3352c006b4c50b60aad5b30a7c760096cbf8f
Adam McElhinney <adam.m.mcelhinney@gmail.com>
2013-09-22 17:24:33 -0500

    finished gen_identity class

--- src/gen_identity.py
+++ src/gen_identity.py
@@ -12,44 +12,61 @@ sys.path.append('/home/amcelhinney/Documents/github_personal/real_estate/src')
 from get_user_agents import get_user_agents, verify_user_agent
 from verify_ip import check_ip_tor
 from random import choice
-
+from get_proxies import get_proxies
 
 class identity:
-    
-    
+
+
     def __init__(self):
-      
+
       # User agents
-      self.user_agents_table = None    
+      self.user_agents_table = None
       self.current_user_agent = None
-      
+
       # Proxies
       self.proxy_table = None
       self.current_proxy = None
 
-    
+    # User agents
     def populate_user_agents(self):
         self.user_agents_table = get_user_agents()
-        
+
     def change_user_agent(self):
         assert self.user_agents_table is not None, "Must call " \
         "populate_user_agents prior to running this method."
         self.current_user_agent = choice(self.user_agents_table)
-        
-        
-    
+
+    # Proxies
+    def populate_proxies(self, filePath):
+        self.proxy_table = get_proxies(filePath)
+
+    def change_proxy(self):
+        assert self.proxy_table is not None, "Must call " \
+        "populate_proxies prior to running this method."
+        self.current_proxy = choice(self.proxy_table)
 
 
 
+    def change_identity(self):
+        '''
+        Helper function to change both proxy and user agent
+        '''
+        self.change_proxy()
+        self.change_user_agent()
 
 
 
 if __name__ == '__main__':
-    
+
     i = identity()
     i.populate_user_agents()
     i.change_user_agent()
-    
-    
-    
-    
+    print i.current_user_agent
+    i.populate_proxies('C:\Users\Adam\Downloads\_reliable_list.txt')
+    i.change_proxy()
+    print i.current_proxy
+    i.change_identity()
+    print i.current_proxy
+    print i.current_user_agent
+
+
---
+++ src/get_proxies.py
@@ -0,0 +1,23 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Adam
+#
+# Created:     22/09/2013
+# Copyright:   (c) Adam 2013
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def get_proxies(filePath):
+    """
+    Gets the proxies from the txt file located at the specified file path
+    """
+    openfile = open(filePath, 'r')
+    t = openfile.read().split('\n')
+    # Last element is just an empty string, so remove that
+    return t[0:len(t)-1]
+
+if __name__ == '__main__':
+    filePath = 'C:\Users\Adam\Downloads\_reliable_list.txt'
+    paths = get_proxies(filePath)
